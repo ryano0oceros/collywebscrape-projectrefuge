1e3c03ee75afecda1286bc2dcdb0d5807e0f208b
ryano0oceros <ryanfallon@microsoft.com>
2024-04-28 15:56:42 -0400

    first functional

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/gocolly/colly"
+)
+
+type Page struct {
+	URL  string `json:"url"`
+	Text string `json:"text"`
+}
+
+func main() {
+	urls := []string{
+		"https://en.wikipedia.org/wiki/Robotics",
+		"https://en.wikipedia.org/wiki/Robot",
+		"https://en.wikipedia.org/wiki/Reinforcement_learning",
+		"https://en.wikipedia.org/wiki/Robot_Operating_System",
+		"https://en.wikipedia.org/wiki/Intelligent_agent",
+		"https://en.wikipedia.org/wiki/Software_agent",
+		"https://en.wikipedia.org/wiki/Robotic_process_automation",
+		"https://en.wikipedia.org/wiki/Chatbot",
+		"https://en.wikipedia.org/wiki/Applications_of_artificial_intelligence",
+		"https://en.wikipedia.org/wiki/Android_(robot)",
+	}
+
+	c := colly.NewCollector()
+
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		page := Page{
+			URL:  e.Request.URL.String(),
+			Text: e.ChildText("p"),
+		}
+
+		file, err := os.OpenFile("output.jsonl", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close()
+
+		encoder := json.NewEncoder(file)
+		if err := encoder.Encode(page); err != nil {
+			fmt.Println("Error encoding JSON:", err)
+		}
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	for _, url := range urls {
+		c.Visit(url)
+	}
+}
